13f922e3009e50ac54f7a3db822658c1a6262236
Juan Gabriel Lopez <juangabriel.lopezsr@gmail.com>
2021-05-03 16:56:37 -0500

    add atributo proveedor

--- modulo.py
+++ modulo.py
@@ -2,5 +2,6 @@ for i in range(1,11):
     print("hola")
 
 class Editor:
-    def __init__(self, nombre):
+    def __init__(self, nombre, proveedor):
         self.__nombre= nombre
+        self.__proveedor = proveedor
