25ef9ab405a99fcef2807e26687fc591723de638
Juan Gabriel Lopez <juangabriel.lopezsr@gmail.com>
2021-05-03 17:24:12 -0500

    add atributo precio

--- modulo.py
+++ modulo.py
@@ -2,6 +2,7 @@ for i in range(1,11):
     print("hola")
 
 class Editor:
-    def __init__(self, nombre, proveedor):
+    def __init__(self, nombre, proveedor,precio):
         self.__nombre= nombre
         self.__proveedor = proveedor
+        self.__precio = precio
